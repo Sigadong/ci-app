ff131206f06323736cb7278c04081a392c61ba93
Sigadong <irwanto.regar30@gmail.com>
2021-02-10 01:14:11 +0700

    Initial commit, php mvc

---
+++ app/models/User_model.php
@@ -0,0 +1,9 @@
+<?php 
+
+class User_model {
+	private $nama = "Sigadong";
+
+	public function getUser() {
+		return $this->nama;
+	}
+}
---
+++ app/views/about/index.php
@@ -0,0 +1,5 @@
+<div class="container">
+	<h1 class="mt-4">About Me</h1>
+	<img src="<?= BASEURL; ?>/img/profil.png" alt="Profil" class="rounded-circle shadow" style="width: 200px">
+	<p>Halo, Nama saya <?= $data['nama']; ?>, umur saya <?= $data['umur']; ?> tahun, saya adalah seorang <?= $data['pekerjaan']; ?></p>
+</div>
